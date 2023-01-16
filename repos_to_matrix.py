cf13fb345f33a7c84013193b50c5c8a7dd171293
René Fritze <rene.fritze@wwu.de>
2023-01-16 18:17:56 +0100

    A new start

---
+++ repos_to_matrix.py
@@ -0,0 +1,15 @@
+import json
+
+with open('repos.json') as f:
+    repos = json.load(f)
+
+print('{"include" : [')
+items = list(repos.items())
+def _println(repo, url):
+    slug = repo.replace('-', '_')
+    print(f'  {{ "module_name": "{repo}", "url": "{url}", "keyname": "{slug}"}} ', end='')
+for repo,url in items[:-1]:
+    _println(repo, url)
+    print(',', end='')
+_println(items[-1][0], items[-1][1])
+print(']}')
